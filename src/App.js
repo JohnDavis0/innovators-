f3ac18f0a9284280a86f219cc0fa2fe8b3f4b87e
alandavis2 <alandavisck@gmail.com>
2023-11-07 12:06:22 -0500

    fronted user

--- src/App.js
+++ src/App.js
@@ -1,37 +1,26 @@
-import './App.css'
-import React from 'react';
-import NavBar from './component/NavBar/navabar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './component/NavBar/Login'; // Import the Login component
-import { Navigate } from 'react-router-dom';
-import Registration from './component/NavBar/Registration';
-import contactlist from './component/NavBar/contactlist';
-import addcontact from './component/NavBar/addcontact';
-import editcontactlist from './component/NavBar/editcontactlist';
-import viewcontact from './component/NavBar/viewcontact';
-import app from './App.css';
-function App() {
-  return (
-    <Router>
-      <React.Fragment>
-        <NavBar />
-        <Routes>
-          <Route path={'/'} element={<Navigate to={'/login'} />} />
-          <Route path={'/login'} element={<Login />} /> {/* Add this line */}
-          <Route path={'/registration'} element={<Registration />} />
-<<<<<<< HEAD
-          <Route path={'/addcontact'} element = {<addcontact />} />
-          <Route path={'/contactlist'} element = {<contactlist />} />
-          <Route path={'/editcontactlist'} element = {<editcontactlist />} />
-          <Route path={'/viewcontact'} element = {<viewcontact />} />
-=======
-          
->>>>>>> f5a94f377b6c8ff956ad07689ed2dd097edaffa0
-          {/* Other routes for your contact manager */}
-        </Routes>
-      </React.Fragment>
-    </Router>
-  );
-}
+import React from "react";
+import "./styles.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import ContactList from "./components/contacts/ContactList/ContactList";
+import AddContact from "./components/contacts/AddContact/AddContact";
+import ViewContact from "./components/contacts/ViewContact/ViewContact";
+import EditContact from "./components/contacts/EditContact/EditContact";
+import Spinner from "./components/Spinner/Spinner";
 
+
+let App = () => {
+  return (
+    <React.Fragment>
+      <NavBar />
+      <Routes>
+        <Route path={"/"} element={<Navigate to={'/contacts/list'}/>}/>
+        <Route path={"/contacts/list"} element={<ContactList/>}/>
+        <Route path={"/contacts/add"} element={<AddContact/>}/>
+        <Route path={"/contacts/view/:contactId"} element={<ViewContact/>}/>
+        <Route path={"/contacts/edit/:contactId"} element={<EditContact/>}/>
+      </Routes>
+    </React.Fragment>
+  );
+};
 export default App;
