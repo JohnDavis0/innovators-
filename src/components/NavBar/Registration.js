cf647321c9be7afd1d4df1e57c5e13838e8c9397
JohnDavis0 <114610727+JohnDavis0@users.noreply.github.com>
2023-11-14 02:18:58 +0530

    Registration updated

--- src/components/NavBar/Registration.js
+++ src/components/NavBar/Registration.js
@@ -4,7 +4,6 @@ import { useNavigate } from 'react-router-dom';
 import supabase from '../../backend/supabase';
 import './Loginpage.css';
 
-
 function Registration() {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
@@ -16,65 +15,93 @@ function Registration() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
-    // Directly insert a new row into the UserAuth table
-    const { data, error } = await supabase
-      .from('UserAuth')
-     .upsert([
-    { username: formData.username, password: formData.password },
-    ], {
-    onConflict: ['username'],
-  });
 
-if (error) {
-  alert('User with the same username already exists. Please choose a different username.');
-  return;
-}
+    // Check if the username is empty
+    if (formData.username.trim() === '') {
+      alert('Username cannot be empty.');
+      return;
+    }
+
+    // Directly insert a new row into the UserAuth table
+    try {
+      const { data, error } = await supabase
+        .from('UserAuth')
+        .upsert(
+          [
+            { username: formData.username, password: formData.password },
+          ],
+          {
+            onConflict: ['username'],
+          }
+        );
   
-    // Redirect to a success page or perform other actions.
-    navigate('/login');
+      if (error) {
+        alert('Username Issue:', error);
+        return;
+      }
+  
+      // Redirect to a success page or perform other actions.
+      navigate('/login');
+    } catch (error) {
+      alert('Username Issue:', error);
+      
+    }
   };
+  
+  
+  
+  
+  
+  
+  
+
+  
   
  
   return (
     <div className="center">
-    <form class="form" style={{ backgroundColor: '#212529' }} onSubmit={handleSubmit}>
-      {/* Your registration form fields go here */}
+    <form className="form" style={{ backgroundColor: '#212529', maxWidth: '400px', margin: '0 auto', padding: '20px', borderRadius: '8px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', textAlign: 'center' }} onSubmit={handleSubmit}>
+  <p id="heading" style={{ fontSize: '24px', fontWeight: 'bold', color: '#fff', marginBottom: '20px' }}>Registration</p>
+  
+  <div className="field" style={{ marginBottom: '15px' }}>
+    <input
+      type="text"
+      name="username"
+      value={formData.username}
+      onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+      placeholder="Username"
+      style={{ width: '90%', padding: '10px', borderRadius: '5px', border: '1px solid #ced4da' }}
+    />
+  </div>
+
+  <div className="field" style={{ marginBottom: '15px' }}>
+    <input
+      type="email"
+      name="email"
+      value={formData.email}
+      onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+      placeholder="Email"
+      style={{ width: '90%', padding: '10px', borderRadius: '5px', border: '1px solid #ced4da' }}
+    />
+  </div>
+
+  <div className="field" style={{ marginBottom: '20px' }}>
+    <input
+      type="password"
+      name="password"
+      value={formData.password}
+      onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+      placeholder="Password"
+      style={{ width: '90%', padding: '10px', borderRadius: '5px', border: '1px solid #ced4da' }}
+    />
+  </div>
+
+  <div className="text-center mt-3">
+    <button type="submit" onClick={handleSubmit} className="button1" style={{ backgroundColor: '#007bff', color: '#fff', padding: '10px 20px', borderRadius: '5px', cursor: 'pointer' }}>Register</button>
+  </div>
+</form>
 
 
-      <p id="heading">Registration</p>
-      <div class="field">
-        <input
-          type="text"
-          name="username"
-          value={formData.username}
-          onChange={(e) => setFormData({ ...formData, username: e.target.value })}
-          placeholder="Username"
-        />
-      </div>
-      <div class="field">
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-          placeholder="Email"
-        />
-      </div>
-      <div class="field">
-        <input
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={(e) => setFormData({ ...formData, password: e.target.value })}
-          placeholder="Password"
-        />
-      </div>
-      {/* Add other registration fields here */}
-      <div class="text-center mt-3">
-        <button type="submit" onClick={handleSubmit} className="button1">Register</button>
-      </div>
-    </form>
   </div>
   );
 }
